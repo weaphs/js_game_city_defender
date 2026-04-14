eec5a902c31105115a1eb2475632dd68d5a3b8bc
Hryhorii <grigorijsilin@gmail.com>
2026-04-14 13:14:58 +0300

    Повністю робоча гра

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.querySelector("#myCanvas");
 const ctx = canvas.getContext("2d");
-const style = getComputedStyle(canvas);
-canvas.width = parseInt(style.width);
-canvas.height = parseInt(style.height);
 
 /** @type {HTMLCanvasElement} */
-const collisionCanvas = document.querySelector("#myCanvas2");// canvas для детекції зіткнень
+const collisionCanvas = document.querySelector("#myCanvas2");
 const collisionCtx = collisionCanvas.getContext("2d");
-collisionCanvas.width = parseInt(style.width);
-collisionCanvas.height = parseInt(style.height);
 
-const backgroundImage = new Image(); // об'єкт зображення фону
-backgroundImage.src = "images/kyiv_background.png"; //шлях до зображення фону
+const backgroundImage = new Image();
+backgroundImage.src = "images/kyiv_background.png";
 
-const backgroundImageMobile = new Image(); // об'єкт зображення фону для мобільної версії
-backgroundImageMobile.src = "images/kyiv_background_mobile.png"; //шлях до зображення фону для мобільної версії
+const backgroundImageMobile = new Image();
+backgroundImageMobile.src = "images/kyiv_background_mobile.png";
 
 const sound_of_shot = new Audio();
 sound_of_shot.src = 'audio/shot.mp3';
 
+canvas.style.cursor = "none";
 
-canvas.style.cursor = "none"; //приховати стандартне зображення курсора мишки
 const crosshair = new Image();
-crosshair.src = "images/crosshair.png";// зображення прицілу для курсора миші
-let mouseX = 0; // координати для руху прицілу
+crosshair.src = "images/crosshair.png";
+
+let mouseX = 0;
 let mouseY = 0;
 
-
-let gameFrame = 0; // номер кадру
-let gameSpeed = 3; // швидкість руху шахеду
-let number_of_tracks = 7; // кількість доріжок для шахедів 
-let score = 0; // кількість збитих шахедів
-let bestScore = 0; // рекорд
-let hits = 0; //кількість влучань в місто
+let gameFrame = 0;
+let gameSpeed = 3;
+let number_of_tracks = 7;
+let score = 0;
+let bestScore = 0;
+let hits = 0;
 let gameOver = false;
 let animationId;
 
@@ -46,9 +41,31 @@ let explosions = [];
 let building_explosions = [];
 
 
+function getCanvasMousePos(canvasElement, clientX, clientY) {
+    const rect = canvasElement.getBoundingClientRect();
+    let x = (clientX - rect.left) / rect.width * canvasElement.width;
+    let y = (clientY - rect.top) / rect.height * canvasElement.height;
+    return { x, y };
+}
+
+function resizeCanvases() {
+    const container = document.querySelector('.canvas_container');
+    const rect = container.getBoundingClientRect();
+
+    canvas.width = collisionCanvas.width = rect.width;
+    canvas.height = collisionCanvas.height = rect.height;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector("#startButton");
-    startButton.addEventListener("click", ()=>{
+
+    resizeCanvases();// Початкове встановлення розмірів
+
+    window.addEventListener('resize', () => {
+        resizeCanvases();
+    });
+
+    startButton.addEventListener("click", () => {
         cancelAnimationFrame(animationId);
         gameOver = false;
         shahed_array = [];
@@ -56,11 +73,11 @@ document.addEventListener("DOMContentLoaded", () => {
         building_explosions = [];
         hits = 0;
         score = 0;
-        animate(0);});
+        animate(0);
+    });
 
-
-    class Explosion { //клас для вибуху при збитті шахеду
-        constructor(x, y, size){
+    class Explosion {
+        constructor(x, y, size) {
             this.x = x;
             this.y = y;
             this.spriteWidth = 200;
@@ -76,33 +93,26 @@ document.addEventListener("DOMContentLoaded", () => {
             this.sound.src = "audio/hit.mp3";
             this.markedForDelition = false;
             this.soundPlayed = false;
-
         }
-        update(deltaTime){
-            if(this.frame === 0 && this.soundPlayed === false){
-                this.sound.play();
-                this.sound.play().catch(() => {});
+        update(deltaTime) {
+            if (this.frame === 0 && !this.soundPlayed) {
+                this.sound.play().catch(() => { });
                 this.soundPlayed = true;
             }
-            this.timeSinceFrame+=deltaTime;
-            if(this.timeSinceFrame>this.frameInterval){
+            this.timeSinceFrame += deltaTime;
+            if (this.timeSinceFrame > this.frameInterval) {
                 this.frame++;
-                this.timeSinceFrame=0;
-                if(this.frame>this.maxFrame){
-                    this.markedForDelition=true;
-
-                }
+                this.timeSinceFrame = 0;
+                if (this.frame > this.maxFrame) this.markedForDelition = true;
             }
-           
         }
-        draw(){
-            ctx.drawImage(this.image, this.spriteWidth*this.frame, 0, this.spriteWidth, this.spriteHeight,
-                this.x, this.y, this.size, this.size
-            )
+        draw() {
+            ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight,
+                this.x, this.y, this.size, this.size);
         }
     }
 
-    class BuildingsExplosion {  //клас для вибуху при падінні шахеда
+    class BuildingsExplosion {
         constructor(x, y, size) {
             this.x = x;
             this.y = y;
@@ -119,11 +129,9 @@ document.addEventListener("DOMContentLoaded", () => {
             this.sound.src = "audio/burst.mp3";
             this.markedForDelition = false;
             this.soundPlayed = false;
-
         }
         update(deltaTime) {
-            if (this.frame === 0 && this.soundPlayed === false) {
-                this.sound.play();
+            if (this.frame === 0 && !this.soundPlayed) {
                 this.sound.play().catch(() => { });
                 this.soundPlayed = true;
             }
@@ -131,152 +139,159 @@ document.addEventListener("DOMContentLoaded", () => {
             if (this.timeSinceFrame > this.frameInterval) {
                 this.frame++;
                 this.timeSinceFrame = 0;
-                if (this.frame > this.maxFrame) {
-                    this.markedForDelition = true;
-
-                }
+                if (this.frame > this.maxFrame) this.markedForDelition = true;
             }
-
         }
         draw() {
             ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight,
-                this.x, this.y, this.size, this.size
-            )
+                this.x, this.y, this.size, this.size);
         }
     }
 
     class Shahed {
         constructor() {
             this.image = new Image();
-            this.image.src = "images/shahed.png"; // шлях до зображення шахеду
-            this.spriteWidth = 1024; // оригінальна ширина шахеду
-            this.spriteHeight = 994;//оригінальна довжина шахеда
-            this.sizeModifier = Math.random()*0.6+0.4;
-            this.width = (canvas.width*this.sizeModifier)/ number_of_tracks;// ширина шахеду на полотні
-            this.height = this.width;// довжина шахеду на полотні
+            this.image.src = "images/shahed.png";
+            this.spriteWidth = 1024;
+            this.spriteHeight = 994;
+            this.sizeModifier = Math.random() * 0.6 + 0.4;
+            this.width = (canvas.width * this.sizeModifier) / number_of_tracks;
+            this.height = this.width;
             this.x = Math.floor(Math.random() * number_of_tracks) * this.width;
-            this.y = -100; 
-            this.speed = Math.random()*gameSpeed+1; // швидкість шахеду
+            this.y = -100;
+            this.speed = Math.random() * gameSpeed + 1;
             this.markedForDelition = false;
-            this.randomColors = [Math.floor(Math.random() * 255), // випадковий колір для детекції зіткнень на myCanvas2
-                 Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
-            this.color = "rgb(" + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';                     
+            this.randomColors = [
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255)
+            ];
+            this.color = `rgb(${this.randomColors[0]},${this.randomColors[1]},${this.randomColors[2]})`;
         }
         update() {
-            if (this.y > canvas.height*0.70){
+            if (this.y > canvas.height * 0.70) {
                 this.markedForDelition = true;
                 building_explosions.push(new BuildingsExplosion(this.x, this.y, this.width));
                 hits++;
-            };
+            }
             this.y += this.speed;
         }
         draw() {
-            collisionCtx.fillStyle= this.color;
+            collisionCtx.fillStyle = this.color;
             collisionCtx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight,
+                this.x, this.y, this.width, this.height);
         }
-
     }
 
-    function drawScore(){
+    function drawScore() {
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
         ctx.textAlign = "left";
-        ctx.fillText('Score: ' + score, 55, 100);
-    };
+        ctx.fillText('Score: ' + score, canvas.width*0.05, 100);
+    }
 
     function drawBestScore() {
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
         ctx.textAlign = "left";
-        ctx.fillText('Best score: ' + bestScore, 55, 55);
-    };
+        ctx.fillText('Best score: ' + bestScore, canvas.width * 0.05, 55);
+    }
 
-    function drawGameOver(){
+    function drawGameOver() {
         ctx.fillStyle = "black";
         ctx.textAlign = "center";
         ctx.font = "40px Arial";
-        ctx.fillText('GAME OVER', canvas.width/2, canvas.height/2);
-    };
+        ctx.fillText('GAME OVER', canvas.width / 2, canvas.height / 2);
+    }
+
 
     canvas.addEventListener("mousemove", (e) => {
-        const rect = canvas.getBoundingClientRect();
-        mouseX = e.clientX - rect.left;
-        mouseY = e.clientY - rect.top;
+        const pos = getCanvasMousePos(canvas, e.clientX, e.clientY);
+        mouseX = pos.x;
+        mouseY = pos.y;
+    });
+
+    window.addEventListener('click', e => {
+        const pos = getCanvasMousePos(collisionCanvas, e.clientX, e.clientY);
+        const clickX = Math.floor(pos.x);
+        const clickY = Math.floor(pos.y);
+
+        const detectPixelColor = collisionCtx.getImageData(clickX, clickY, 1, 1);
+        const pc = detectPixelColor.data;
+
+        let hit = false;
+
+        shahed_array.forEach(object => {
+            if (object.randomColors[0] === pc[0] &&
+                object.randomColors[1] === pc[1] &&
+                object.randomColors[2] === pc[2]) {
+                object.markedForDelition = true;
+                score++;
+                explosions.push(new Explosion(object.x, object.y, object.width));
+                hit = true;
+            }
+        });
+
+        if (!hit) {
+            const rect = collisionCanvas.getBoundingClientRect();
+            if (e.clientX > rect.left && e.clientX < rect.right &&
+                e.clientY > rect.top && e.clientY < rect.bottom) {
+                sound_of_shot.play().catch(() => { });
+            }
+        }
     });
 
     function drawCursor() {
-        let crosshairSize = 128;
+        const crosshairSize = 128;
         ctx.drawImage(
             crosshair, 0, 0, 1024, 1024,
-            mouseX - (crosshairSize/2), mouseY - (crosshairSize/2), crosshairSize, crosshairSize
+            mouseX - (crosshairSize / 2),
+            mouseY - (crosshairSize / 2),
+            crosshairSize, crosshairSize
         );
-
     }
 
-    window.addEventListener('click', e => {
-        const rect = collisionCanvas.getBoundingClientRect(); // рамки об'єкта canvas
-        const clickX = e.clientX - rect.left; //дає х координату відносно canvas
-        const clickY = e.clientY - rect.top; //дає y координату відносно canvas
-        const detectPixelColor = collisionCtx.getImageData(clickX, clickY, 1, 1);
-        const pc = detectPixelColor.data; // колір пікселя на який був клік
-        shahed_array.forEach(object=>{
-            if(object.randomColors[0]===pc[0]&&
-                object.randomColors[1] === pc[1] &&
-                object.randomColors[2] === pc[2]
-            ){object.markedForDelition=true;
-            score++;
-            explosions.push(new Explosion(object.x, object.y, object.width));
-            } else if (e.clientX > rect.x && e.clientX < rect.x+rect.width &&
-                e.clientY > rect.y && e.clientY < rect.y+rect.width
-            ){
-                sound_of_shot.play();
-                sound_of_shot.play().catch(() => { });
-            }
-        })
-    });
-
-    function animate(timestamp) {
+    function animate(timestamp = 0) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
-        if (window.matchMedia("(min-width: 1024px)").matches){
+
+        if (window.matchMedia("(min-width: 1024px)").matches) {
             ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
             drawCursor();
-        }else{
+        } else {
             ctx.drawImage(backgroundImageMobile, 0, 0, canvas.width, canvas.height);
         }
+
         let deltaTime = timestamp - lastTime;
         lastTime = timestamp;
+
         timeToNewShahed += deltaTime;
-        if (timeToNewShahed > shahedInterval){
+        if (timeToNewShahed > shahedInterval) {
             shahed_array.push(new Shahed());
-            shahed_array.sort(function(a,b){
-                return a.width-b.width;
-            })
+            shahed_array.sort((a, b) => a.width - b.width);
             timeToNewShahed = 0;
         }
-        [...shahed_array, ...explosions, ...building_explosions].forEach(object=>{
-            object.update(deltaTime);
-        });
-        [...shahed_array, ...explosions, ...building_explosions].forEach(object => {
-            object.draw();
-        });
-        shahed_array = shahed_array.filter(shahed => !shahed.markedForDelition);
-        explosions = explosions.filter(explosion => !explosion.markedForDelition);
-        building_explosions = building_explosions.filter(explosion => !explosion.markedForDelition);
+
+        [...shahed_array, ...explosions, ...building_explosions].forEach(obj => obj.update(deltaTime));
+        [...shahed_array, ...explosions, ...building_explosions].forEach(obj => obj.draw());
+
+        shahed_array = shahed_array.filter(s => !s.markedForDelition);
+        explosions = explosions.filter(e => !e.markedForDelition);
+        building_explosions = building_explosions.filter(e => !e.markedForDelition);
+
         drawBestScore();
         drawScore();
-        if(hits>3){
-            if (score > bestScore) {
-                bestScore = score;
-            }
-            gameOver=true;
-        };
+
+        if (hits > 3) {
+            if (score > bestScore) bestScore = score;
+            gameOver = true;
+        }
+
         if (!gameOver) {
             animationId = requestAnimationFrame(animate);
         } else {
             drawGameOver();
         }
- 
     }
 });
